5897cad085816051aa2871216fa8f7ccc66ecbf9
MSI-PC <MSI-PC@DSPADE-MSI>
2016-11-09 13:40:42 +0300

    Добавьте файлы проекта.

---
+++ Ifrit/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ifrit.Startup))]
+namespace Ifrit
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
